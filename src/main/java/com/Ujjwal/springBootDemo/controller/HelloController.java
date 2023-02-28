162588f9434ebfcfe1bba3ba0b53f11b42851f10
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-02-28 09:55:23 +0530

    added h2 db

---
+++ src/main/java/com/Ujjwal/springBootDemo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.Ujjwal.springBootDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Component
+//@Controller
+@RestController
+public class HelloController {
+	
+	//@RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
+	@GetMapping(value = "/helloWorld")
+	public String helloWorld() {
+		return "Welcome to Ujjwal's demo change again";
+	}
+
+}
