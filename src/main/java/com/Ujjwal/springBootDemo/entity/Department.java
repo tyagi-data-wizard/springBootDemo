154361b25b3948f0392dd0c4dc402ec42c7e170f
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-03-04 22:59:35 +0530

    Adding department entity

---
+++ src/main/java/com/Ujjwal/springBootDemo/entity/Department.java
@@ -0,0 +1,33 @@
+package com.Ujjwal.springBootDemo.entity;
+
+public class Department {
+	private Long departmentIdLong;
+	private String departmentNameg;
+	private String departmentAddress;
+	private String departmentCode;
+	public Long getDepartmentIdLong() {
+		return departmentIdLong;
+	}
+	public void setDepartmentIdLong(Long departmentIdLong) {
+		this.departmentIdLong = departmentIdLong;
+	}
+	public String getDepartmentNameg() {
+		return departmentNameg;
+	}
+	public void setDepartmentNameg(String departmentNameg) {
+		this.departmentNameg = departmentNameg;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+
+}
