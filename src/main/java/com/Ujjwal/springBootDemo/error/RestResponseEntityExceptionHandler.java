cd7ff3db46fbb67ac9b49deb16590b5903ec1f76
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-03-26 00:34:11 +0530

    dummy commit

--- src/main/java/com/Ujjwal/springBootDemo/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/Ujjwal/springBootDemo/error/RestResponseEntityExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+
     @ExceptionHandler(DepartmentNotFoundException.class)
     public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException exception,
                                                                    WebRequest webRequest){
