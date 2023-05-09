2d41f4b152f52dd480ba2adab1bc220c21e12272
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-05-09 13:15:07 +0530

    changed postgresql version & config

--- src/main/java/com/Ujjwal/springBootDemo/repository/DepartmentRepository.java
+++ src/main/java/com/Ujjwal/springBootDemo/repository/DepartmentRepository.java
@@ -1,16 +1,27 @@
 package com.Ujjwal.springBootDemo.repository;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import com.Ujjwal.springBootDemo.entity.Department;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long>{
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
 	public Department findByDepartmentName(String departmentName);
-	
+
 	//if we want to give a sql query we can use @Query annotation as well
 	//e.g. @Query(value="SQL_QUERY", nativeQuery = "true")// can give parameters as ?1 & ?2
 	public Department findByDepartmentNameIgnoreCase(String departmentName);
+
 }
+
