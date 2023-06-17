08862df63d50cb7167a84f0aeb09dca4929bc4ce
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-17 13:33:50 +0530

    converting prop to yml file

--- src/main/java/com/Ujjwal/springBootDemo/controller/HelloController.java
+++ src/main/java/com/Ujjwal/springBootDemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.Ujjwal.springBootDemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 @RestController
 public class HelloController {
+
+	@Value("${welcome.message}")
+	private String welcomeMessage;
 	
 	//@RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
 	@GetMapping(value = "/helloWorld")
 	public String helloWorld() {
-		return "Welcome to Ujjwal's demo change again and again";
+		return welcomeMessage;
 		
 	}
 
